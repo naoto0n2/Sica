19b20412ce9e90e0a3ef7dd6cb4d890e1c9986fb
Naoto Onagi <onagi@awa.fm>
2025-08-04 16:13:10 +0900

    Remove conditional typealias for LaunchOptionsKey to simplify code

--- Demo/SicaDemo/AppDelegate.swift
+++ Demo/SicaDemo/AppDelegate.swift
@@ -13,11 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    #if swift(>=4.2)
     typealias LaunchOptionsKey = UIApplication.LaunchOptionsKey
-    #else
-    typealias LaunchOptionsKey = UIApplicationLaunchOptionsKey
-    #endif
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
